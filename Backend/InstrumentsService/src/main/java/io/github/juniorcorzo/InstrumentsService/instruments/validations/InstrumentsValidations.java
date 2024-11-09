bef44b8f4f465f1f90ff4fab963c6731c2ff75fc
Junior Corzo <josedanielmmf@gmail.com>
2024-11-08 22:12:28 -0500

    ADD: Se creo InstrumentsValidations en el paquete validations para separar la logica de validacion del InstrumentsService

---
+++ Backend/InstrumentsService/src/main/java/io/github/juniorcorzo/InstrumentsService/instruments/validations/InstrumentsValidations.java
@@ -0,0 +1,34 @@
+package io.github.juniorcorzo.InstrumentsService.instruments.validations;
+
+import io.github.juniorcorzo.InstrumentsService.instruments.exceptions.InstrumentIdNotFound;
+import io.github.juniorcorzo.InstrumentsService.instruments.repositories.InstrumentsRepository;
+import io.github.juniorcorzo.InstrumentsService.instruments.service.InstrumentsService;
+import io.github.juniorcorzo.InstrumentsService.shared.exception.FormatIdNotValid;
+import lombok.AllArgsConstructor;
+import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class InstrumentsValidations {
+    private final InstrumentsRepository instrumentsRepository;
+    private final Logger LOGS = LoggerFactory.getLogger(InstrumentsValidations.class);
+
+    private void validIdFormat(String id){
+        boolean isValid = ObjectId.isValid(id);
+        if (!isValid) {
+            LOGS.error("ID is not valid");
+            throw new FormatIdNotValid();
+        }
+    }
+
+    public void validIdWithInstrumentExists(String id) {
+        validIdFormat(id);
+        if (!this.instrumentsRepository.existsById(id)) {
+            LOGS.error("Instruments with {} not exists", id);
+            throw new InstrumentIdNotFound();
+        }
+    }
+}
