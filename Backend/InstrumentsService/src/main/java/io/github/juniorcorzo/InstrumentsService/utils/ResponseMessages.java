a960e0c59405bffcd0f8e3ac8e342f97f0297395
Junior Corzo <josedanielmmf@gmail.com>
2024-11-08 19:30:00 -0500

    REFACTOR: Se añadio el mensaje para la excepcion de brands

--- Backend/InstrumentsService/src/main/java/io/github/juniorcorzo/InstrumentsService/utils/ResponseMessages.java
+++ Backend/InstrumentsService/src/main/java/io/github/juniorcorzo/InstrumentsService/utils/ResponseMessages.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 public enum ResponseMessages {
     OK("Operación completada existosamente"),
     INSTRUMENT_ID_NOT_FOUND("Id de intrumento no se encuentra registrado"),
+    BRAND_ID_NOT_FOUND("Id de la marca no se encuentra registrado"),
     FORMAT_ID_NOT_VALID("El formato del id no es valido");
+
     private final String message;
     ResponseMessages(String message) {
         this.message = message;
