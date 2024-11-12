54b91b2e3c65243da21d5fe1be2d110321d0f06d
Junior Corzo <josedanielmmf@gmail.com>
2024-11-11 21:39:55 -0500

    REFACTOR: Se acomodo el orden de la repuesta

--- Backend/TagsInstrumentsService/src/main/java/io/github/juniorcorzo/tagsinstrumentsservice/tags/models/Alarms.java
+++ Backend/TagsInstrumentsService/src/main/java/io/github/juniorcorzo/tagsinstrumentsservice/tags/models/Alarms.java
@@ -1,9 +1,14 @@
 package io.github.juniorcorzo.tagsinstrumentsservice.tags.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
+@JsonPropertyOrder({"hh", "h", "l", "ll"})
 @AllArgsConstructor
 @Data
 public class Alarms {
