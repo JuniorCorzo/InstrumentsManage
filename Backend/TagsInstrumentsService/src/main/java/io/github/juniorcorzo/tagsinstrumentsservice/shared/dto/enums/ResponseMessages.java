61e45e8e292e1cb26854f1c20a2c339f80867af9
Junior Corzo <josedanielmmf@gmail.com>
2024-11-11 20:17:56 -0500

    REFACTOR: Se añadio el mensaje para formato de id no valido

--- Backend/TagsInstrumentsService/src/main/java/io/github/juniorcorzo/tagsinstrumentsservice/shared/dto/enums/ResponseMessages.java
+++ Backend/TagsInstrumentsService/src/main/java/io/github/juniorcorzo/tagsinstrumentsservice/shared/dto/enums/ResponseMessages.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 public enum ResponseMessages {
     OK("Operación completada exitosamente"),
-    TAG_ID_NOT_FOUND("El id del tag no se encuentra registrado");
+    TAG_ID_NOT_FOUND("El id del tag no se encuentra registrado"),
+    ID_FORMAT_NOT_VALID("Formato del id no es valido");
 
     private final String message;
 }
