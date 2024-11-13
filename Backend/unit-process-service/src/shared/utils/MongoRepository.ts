c86fd2c22cfabcabd58905473ff69f6eba63d25f
Junior Corzo <josedanielmmf@gmail.com>
2024-11-13 16:22:02 -0500

    ADD: Enhanced MongoRepository class with generic ID support and CRUD methods

--- Backend/unit-process-service/src/shared/utils/MongoRepository.ts
+++ Backend/unit-process-service/src/shared/utils/MongoRepository.ts
@@ -1,19 +1,50 @@
 import { Inject } from "@nestjs/common";
-import { Collection, MongoClient } from "mongodb"
+import { Collection, OptionalUnlessRequiredId, WithId } from "mongodb"
 import { ConnectDB } from "src/shared/config/ConnectDB";
 
-export class MongoRepository<T> {
+export class MongoRepository<T, ID> {
     private readonly collection: Collection<T>
-    constructor(@Inject() clientMongo: ConnectDB){
-        this.collection = clientMongo.getConnection().db('unitProcess').collection('unitProcessCollection')
+    constructor(clientMongo: ConnectDB, collectionName: string){
+        this.collection = clientMongo.getConnection()
+                                    .db('unitProcess')
+                                    .collection(collectionName)
     }
 
-    public async findAll(){
+    public async findAll(): Promise<WithId<T>[]>{
         try {
            const data = await this.collection.find().toArray().then(data => data)
            return data;
         } catch (error) {
-            
+            console.error(error)
         }
+    }
+
+    public async findById(id: ID) {
+        try {
+            const data = await this.collection.findOne({_id: id}).then(data => data)
+            return data
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    public async insert(document: OptionalUnlessRequiredId<T>) {
+        try {
+            await this.collection.insertOne(document);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    public async update(document: []) {
+        try {
+            await this.collection.updateOne({_id: document['id']}, document)            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    public async delete(id: ID) {
+        await this.collection.deleteOne({_id: id})
     }
 }
