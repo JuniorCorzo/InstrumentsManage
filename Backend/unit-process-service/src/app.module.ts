00852040f6f6e90e4c0caa0e37939c53eadeea7a
Junior Corzo <josedanielmmf@gmail.com>
2024-11-14 17:21:07 -0500

    REFACTOR: Import CampModule into AppModule

--- Backend/unit-process-service/src/app.module.ts
+++ Backend/unit-process-service/src/app.module.ts
@@ -1,7 +1,8 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UnitProcessModule } from './unitProcess/modules/unit-process.module';
+import { CampModule } from './camp/modules/camp.module';
 
 @Module({
-  imports: [UnitProcessModule],
+  imports: [UnitProcessModule, CampModule],
 })
 export class AppModule {}
