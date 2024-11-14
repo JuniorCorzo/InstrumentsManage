a887edd3e953733e40d73c5f3b5c6b3cd73fbc88
Junior Corzo <josedanielmmf@gmail.com>
2024-11-14 15:00:49 -0500

    REFACTOR: Implement IUnitProcess interface in UnitProcess model for better type definition

--- Backend/unit-process-service/src/unitProcess/model/unit-process.model.ts
+++ Backend/unit-process-service/src/unitProcess/model/unit-process.model.ts
@@ -2,8 +2,14 @@ import { ObjectId } from "mongodb"
 import { camp } from "./camp.model" 
 import { Collection } from "src/common/decorator/collection.decorator"
 
+type IUnitProcess = {
+    id?: ObjectId
+    name: string
+    camp: camp
+}
+
 @Collection("unitProcessCollection")
-export class UnitProcess {
+export class UnitProcess implements IUnitProcess {
     id: ObjectId
     name: string
     camp: camp
