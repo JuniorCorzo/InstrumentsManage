26f9bd0249c6ac006ad9f668f0864b27a2cbc3af
Junior Corzo <josedanielmmf@gmail.com>
2024-11-14 15:01:36 -0500

    REFACTOR: Enhance UnitProcessController with CRUD operations and improve type safety

--- Backend/unit-process-service/src/unitProcess/controller/unit-process.controller.ts
+++ Backend/unit-process-service/src/unitProcess/controller/unit-process.controller.ts
@@ -1,18 +1,37 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { UnitProcessService } from '../service/unit-process.service';
 import { UnitProcess } from '../model/unit-process.model';
+import { runInThisContext } from 'vm';
+import { WithId } from 'mongodb';
 
 @Controller("/unit-process")
 export class UnitProcessController {
   constructor(private readonly unitProcessService: UnitProcessService) {}
 
   @Get("/all")
-  getAllUnitProcess() {
+  public getAllUnitProcess() {
     return this.unitProcessService.getAllUnitProcess();
+  }
+
+  @Get(':id')
+  public getUnitProcessById(@Param('id') id: string){
+    return this.unitProcessService.getUnitProcessById(id)
   }
 
   @Post("/create")
   insertUnitProcess(@Body() unitProcess: UnitProcess) {
      return this.unitProcessService.insertUnitProcess(unitProcess)
   }
+
+  @Put('/update')
+  public updateUnitProcess(@Body() unitProcess: WithId<UnitProcess>) {
+    return this.unitProcessService.updateUnitProcess(unitProcess)
+  }
+
+  @Delete('/delete/:id')
+  public deleteUnitProcess(@Param('id') id: string) {
+    
+    this.unitProcessService.deleteUnitProcess(id)
+  }
 }
+
