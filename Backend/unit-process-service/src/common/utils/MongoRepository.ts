6cb89b95872ac5f4ded5f8f1a3e885589ee531cc
Junior Corzo <josedanielmmf@gmail.com>
2024-11-14 19:16:35 -0500

    FIX: manage of id

--- Backend/unit-process-service/src/common/utils/MongoRepository.ts
+++ Backend/unit-process-service/src/common/utils/MongoRepository.ts
@@ -1,23 +1,23 @@
-import { Inject } from "@nestjs/common";
-import { Collection, Document, ObjectId, OptionalUnlessRequiredId, UpdateFilter, WithId } from "mongodb"
+import { Inject, Injectable, Scope } from "@nestjs/common";
+import { Collection, Document, Filter, ObjectId, OptionalUnlessRequiredId, UpdateFilter, WithId } from "mongodb"
+import { Camp } from "src/camp/model/camp.model";
 import { ConnectDB } from "src/common/config/ConnectDB";
 import { UnitProcess } from "src/unitProcess/model/unit-process.model";
-
 
 export class MongoRepository<T, ID> {
     private readonly collection: Collection<T>
 
-    constructor(@Inject() clientMongo: ConnectDB){
-
+    constructor(@Inject() clientMongo: ConnectDB) {
+        console.log(Reflect.getMetadata('collection_name', this.constructor))
         this.collection = clientMongo.getConnection()
-                                    .db('unitProcess')
-                                    .collection(Reflect.getMetadata('collection_name', UnitProcess))
+            .db('unitProcess')
+            .collection(Reflect.getMetadata('collection_name', this.constructor))
     }
 
-    public async findAll(): Promise<WithId<T>[]>{
+    public async findAll(): Promise<WithId<T>[]> {
         try {
-           const data = await this.collection.find().toArray().then(data => data)
-           return data;
+            const data = await this.collection.find().toArray()
+            return data;
         } catch (error) {
             console.error(error)
         }
@@ -25,7 +25,8 @@ export class MongoRepository<T, ID> {
 
     public async findById(id: ID) {
         try {
-            const data = await this.collection.findOne({_id: id}).then(data => data)
+            const data = await this.collection.findOne({ _id: new ObjectId(id as string) as Filter<T> }).then(data => data)
+            console.log(data)
             return data
         } catch (error) {
             console.error(error)
@@ -42,10 +43,10 @@ export class MongoRepository<T, ID> {
 
     public async update(document: WithId<T>) {
         try {
-            const {_id, ...documentWithoutId} = document
+            const { _id, ...documentWithoutId } = document
             await this.collection.updateOne(
-                    {_id: _id as UpdateFilter<T> },
-                    {$set: documentWithoutId as Partial<T>})
+                { _id: new ObjectId(_id) as Filter<T> },
+                { $set: documentWithoutId as Partial<T> }).then(data => console.log(data))
         } catch (error) {
             console.log(error)
         }
@@ -53,7 +54,7 @@ export class MongoRepository<T, ID> {
 
     public async delete(id: ID) {
         await this.collection
-            .deleteOne({ _id:  new ObjectId(id as string )} as WithId<T>)
-            
+            .deleteOne({ _id: new ObjectId(id as string) } as WithId<T>)
+
     }
 }
