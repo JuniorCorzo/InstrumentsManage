9cd172d8a1ec8d1b03a49da06f2f6554d4cf8c6a
Junior Corzo <josedanielmmf@gmail.com>
2024-11-16 19:44:53 -0500

    FEAT: Enhance UnitProcessValidations to include async ID existence checks and improve error handling

--- Backend/unit-process-service/src/unitProcess/validations/unit-process.validation.ts
+++ Backend/unit-process-service/src/unitProcess/validations/unit-process.validation.ts
@@ -1,26 +1,30 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, NotFoundException, UseFilters } from "@nestjs/common";
 import { ObjectId } from "mongodb";
 import { ResponseMessages } from "src/common/enums/response-messages";
 import { IdNotValid } from "src/common/exceptions/id-not-valid.exception";
 import { UnitProcessRepository } from "../repositories/unit-process.repository";
 import { UnitProcessNotFound } from "../exceptions/unit-process-not-found.exception";
+import { isObservable } from "rxjs";
 
 @Injectable()
 export class UnitProcessValidations {
     constructor(private readonly unitProcessRepository: UnitProcessRepository) { }
 
-    private validId(id: string) {
+    public validId(id: string) {
         if (!ObjectId.isValid(id)) {
             console.error(ResponseMessages.ID_NOT_VALID)
             throw new IdNotValid()
         }
     }
 
-    public validIdByExist(id: string) {
+    public async validIdExist(id: string) {
         this.validId(id)
-        if (!this.unitProcessRepository.existById(id)) {
-            console.error(ResponseMessages.UNIT_PROCESS_NOT_FOUND)
+        try {
+            const isValid: boolean | void = await this.unitProcessRepository.existById(id)
+            if (isValid) throw new UnitProcessNotFound()
+        } catch (err) {
             throw new UnitProcessNotFound()
         }
     }
+
 }
