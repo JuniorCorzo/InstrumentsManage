23f641ac78715c0a9792459863ffd11bd69d3684
Junior Corzo <josedanielmmf@gmail.com>
2024-11-16 19:45:22 -0500

    FEAT: Rename 'id' to '_id' in UnitProcess model for consistency with MongoDB conventions

--- Backend/unit-process-service/src/unitProcess/model/unit-process.model.ts
+++ Backend/unit-process-service/src/unitProcess/model/unit-process.model.ts
@@ -3,14 +3,14 @@ import { Camp } from "../../camp/model/camp.model"
 import { Collection } from "src/common/decorator/collection.decorator"
 
 type IUnitProcess = {
-    id?: ObjectId
+    _id?: ObjectId
     name: string
     camp: Camp
 }
 
 @Collection("unitProcessCollection")
 export class UnitProcess implements IUnitProcess {
-    id: ObjectId
+    _id: ObjectId
     name: string
     camp: Camp
 }
