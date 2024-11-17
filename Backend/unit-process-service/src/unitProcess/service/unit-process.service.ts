c7e4cf9fadeb0e061d3765884fd2ff65f3cd336a
Junior Corzo <josedanielmmf@gmail.com>
2024-11-16 19:45:08 -0500

    FEAT: Refactor UnitProcessService methods to return ResponseDTO and improve async handling

--- Backend/unit-process-service/src/unitProcess/service/unit-process.service.ts
+++ Backend/unit-process-service/src/unitProcess/service/unit-process.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, Res } from '@nestjs/common';
 import { UnitProcessRepository } from 'src/unitProcess/repositories/unit-process.repository';
 import { UnitProcess } from '../model/unit-process.model';
 import { WithId } from 'mongodb';
 import { UnitProcessValidations } from '../validations/unit-process.validation';
+import { ResponseMessages } from 'src/common/enums/response-messages';
+import { ResponseDTO } from 'src/common/dto/response-with-data.dto';
+
 
 @Injectable()
 export class UnitProcessService {
@@ -11,29 +14,55 @@ export class UnitProcessService {
     private readonly unitProcessValidation: UnitProcessValidations
   ) { }
 
-  public getAllUnitProcess(): Promise<WithId<UnitProcess>[]> {
-    return this.unitProcessRepository.findAll();
-  }
-
-  public getUnitProcessById(id: string): Promise<WithId<UnitProcess>> {
-    this.unitProcessValidation.validIdByExist(id)
+  public async getAllUnitProcess(): Promise<ResponseDTO<WithId<UnitProcess>>> {
+    const data = await this.unitProcessRepository.findAll()
     
-    return this.unitProcessRepository.findById(id);
+    return new ResponseDTO(
+      HttpStatus.OK,
+      ResponseMessages.OK,
+      data
+    );
   }
 
-  public insertUnitProcess(unitProcess: UnitProcess): void {
+  public async getUnitProcessById(id: string): Promise<ResponseDTO<UnitProcess>> {
+    await this.unitProcessValidation.validIdExist(id)
+    const data = await this.unitProcessRepository.findById(id)
+
+    return new ResponseDTO(
+      HttpStatus.OK,
+      ResponseMessages.OK,
+      data
+    )
+  }
+
+  //TODO:: Devolver la unidad creada
+  public async insertUnitProcess(unitProcess: UnitProcess): Promise<ResponseDTO<UnitProcess>> {
     this.unitProcessRepository.insert(unitProcess)
+
+    return new ResponseDTO<UnitProcess>(
+      HttpStatus.CREATED,
+      ResponseMessages.OK
+    )
   }
 
-  public updateUnitProcess(unitProcess: WithId<UnitProcess>): void {
-    this.unitProcessValidation.validIdByExist(unitProcess._id.toString())
-    
-    this.unitProcessRepository.update(unitProcess)
+  //TODO:: Devolver la unidad actualizada
+  public async updateUnitProcess(unitProcess: WithId<UnitProcess>): Promise<ResponseDTO<UnitProcess>> {
+    await this.unitProcessValidation.validIdExist(unitProcess._id.toString())
+    await this.unitProcessRepository.update(unitProcess)
+
+    return new ResponseDTO<UnitProcess>(
+      HttpStatus.OK,
+      ResponseMessages.OK
+    )
   }
 
-  public deleteUnitProcess(id: string): void {
-    this.unitProcessValidation.validIdByExist(id)
+  public async deleteUnitProcess(id: string): Promise<ResponseDTO<UnitProcess>> {
+    await this.unitProcessValidation.validIdExist(id)
+    await this.unitProcessRepository.delete(id)
 
-    this.unitProcessRepository.delete(id)
+    return new ResponseDTO<UnitProcess>(
+      HttpStatus.OK,
+      ResponseMessages.OK
+    )
   }
 }
