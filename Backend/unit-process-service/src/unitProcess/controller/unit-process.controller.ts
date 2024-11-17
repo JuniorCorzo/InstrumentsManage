45c40617b148085caba9b34346eb83773ecd429c
Junior Corzo <josedanielmmf@gmail.com>
2024-11-16 19:46:21 -0500

    FEAT: Update UnitProcessController methods to return ResponseDTO for improved type handling

--- Backend/unit-process-service/src/unitProcess/controller/unit-process.controller.ts
+++ Backend/unit-process-service/src/unitProcess/controller/unit-process.controller.ts
@@ -2,35 +2,37 @@ import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
 import { UnitProcessService } from '../service/unit-process.service';
 import { UnitProcess } from '../model/unit-process.model';
 import { WithId } from 'mongodb';
+import { ResponseDTO } from 'src/common/dto/response-with-data.dto';
 
 @Controller("/unit-process")
 export class UnitProcessController {
-  constructor(private readonly unitProcessService: UnitProcessService) {}
+  constructor(
+    private readonly unitProcessService: UnitProcessService,
+  ) { }
 
   @Get("/all")
-  public getAllUnitProcess() {
+  public getAllUnitProcess(): Promise<ResponseDTO<WithId<UnitProcess>>> {
     return this.unitProcessService.getAllUnitProcess();
-  }
+}_
 
   @Get(':id')
-  public getUnitProcessById(@Param('id') id: string){
+  public getUnitProcessById(@Param('id') id: string): Promise<ResponseDTO<WithId<UnitProcess>>> {
     return this.unitProcessService.getUnitProcessById(id)
   }
 
   @Post("/create")
-  insertUnitProcess(@Body() unitProcess: UnitProcess) {
-     return this.unitProcessService.insertUnitProcess(unitProcess)
+  insertUnitProcess(@Body() unitProcess: UnitProcess): Promise<ResponseDTO<WithId<UnitProcess>>> {
+    return this.unitProcessService.insertUnitProcess(unitProcess)
   }
 
   @Put('/update')
-  public updateUnitProcess(@Body() unitProcess: WithId<UnitProcess>) {
+  public updateUnitProcess(@Body() unitProcess: WithId<UnitProcess>): Promise<ResponseDTO<WithId<UnitProcess>>> {
     return this.unitProcessService.updateUnitProcess(unitProcess)
   }
 
   @Delete('/delete/:id')
-  public deleteUnitProcess(@Param('id') id: string) {
-    
-    this.unitProcessService.deleteUnitProcess(id)
+  public deleteUnitProcess(@Param('id') id: string): Promise<ResponseDTO<UnitProcess>> {
+    return this.unitProcessService.deleteUnitProcess(id)
   }
 }
 
