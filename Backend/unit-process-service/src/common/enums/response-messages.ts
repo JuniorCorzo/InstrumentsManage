39e4ed697100c805fd0a7d2fd2a54ec3248afb66
Junior Corzo <josedanielmmf@gmail.com>
2024-11-16 22:31:47 -0500

    FEAT: Add CAMP_NOT_FOUND message to ResponseMessages enum for improved error reporting

--- Backend/unit-process-service/src/common/enums/response-messages.ts
+++ Backend/unit-process-service/src/common/enums/response-messages.ts
@@ -1,6 +1,7 @@
 export enum ResponseMessages {
     OK = 'La operacion fue exitosa',
     ID_NOT_VALID = 'El id no es valido',
-    UNIT_PROCESS_NOT_FOUND = 'La unidad de proceso no se encuentra registrada'
+    UNIT_PROCESS_NOT_FOUND = 'La unidad de proceso no se encuentra registrada',
+    CAMP_NOT_FOUND = 'El campo no se encuentra registrado'
 
 }
