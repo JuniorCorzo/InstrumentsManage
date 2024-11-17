861848624ddbf99b51e1017d640fe9365f2c2710
Junior Corzo <josedanielmmf@gmail.com>
2024-11-16 23:24:14 -0500

    FEAT: Enhance error handling in MongoRepository for insert and update methods

--- Backend/unit-process-service/src/common/repository/mongo-repository.repository.ts
+++ Backend/unit-process-service/src/common/repository/mongo-repository.repository.ts
@@ -2,6 +2,7 @@ import { Inject } from "@nestjs/common";
 import { Collection, Filter, ObjectId, OptionalUnlessRequiredId, WithId } from "mongodb"
 import { ConnectDB } from "src/common/config/ConnectDB";
 
+//TODO:: Mejorar el manejo de excepciones
 export class MongoRepository<T, ID> {
     private readonly collection: Collection<T>
 
@@ -28,21 +29,30 @@ export class MongoRepository<T, ID> {
         }
     }
 
-    public async insert(document: OptionalUnlessRequiredId<T>) {
+    public async insert(document: OptionalUnlessRequiredId<T>): Promise<WithId<T>> {
         try {
-            return await this.collection.insertOne(document)
+           const idInsert: ID = await this.collection.insertOne(document)
+                .then(({insertedId}) => insertedId.toString() as ID)
+            
+            if (!ObjectId.isValid(idInsert as string)) throw new Error("Error al momento de insertar")
+
+            return await this.findById(idInsert)
         } catch (error) {
             console.error(error)
         }
     }
 
-    public async update(document: WithId<T>) {
+    public async update(document: WithId<T>): Promise<WithId<T>>  {
         try {
             const { _id, ...documentWithoutId } = document
+            
             return await this.collection.updateOne(
                 { _id: new ObjectId(_id) as Filter<T> },
                 { $set: documentWithoutId as Partial<T> })
-                    .then(data => console.log(data))
+                    .then(({matchedCount}) => {
+                        if (matchedCount >= 1) return this.findById(_id as ID)
+                        throw new Error("Error al intentar actualizar")
+                    })
         } catch (error) {
             console.error(error)
         }
