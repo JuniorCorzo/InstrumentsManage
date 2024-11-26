31a185e08f331cf81390c3011fc60e6baf3f3f0a
Junior Corzo <josedanielmmf@gmail.com>
2024-11-25 22:24:43 -0500

    FEAT: Mejorado la conexion a la base de datos usando recursividad

--- Backend/unit-process-service/src/common/config/ConnectDB.ts
+++ Backend/unit-process-service/src/common/config/ConnectDB.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleInit } from '@nestjs/common'
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common'
 import { MongoClient } from 'mongodb'
 
 @Injectable()
@@ -6,23 +6,45 @@ export class ConnectDB implements OnModuleInit {
   // eslint-disable-next-line no-use-before-define
   private static instance: ConnectDB
   private clientMongo: MongoClient
+  private readonly MAX_ATTEMPTS = 1
+  private readonly LOGS = new Logger(ConnectDB.name, { timestamp: true })
 
+  // TODO:: fix error gramatical
   private constructor () {
     this.clientMongo = new MongoClient('mongodb://localhost:27019/unitProcces')
   }
 
-  async onModuleInit () {
+  /**
+   * Función asíncrona basada en la recursiva para realizar la conexión a MongoDB
+   *
+   * @returns MongoClient
+   */
+  private async connectDB (attempts?: number): Promise<MongoClient> {
+    attempts = attempts === undefined ? 0 : attempts
     try {
       await this.clientMongo.connect()
-      console.log('Conexion a MongoDB establecida exitosamente')
+      this.LOGS.log('Connected to MongoDB successfully')
+      // eslint-disable-next-line no-useless-return
+      return
     } catch (error) {
-      console.error(error)
+      this.LOGS.error(error.message)
+      if (attempts < this.MAX_ATTEMPTS) {
+        this.LOGS.log('Attempting to establish a connection to MongoDB')
+        console.log(attempts)
+        return this.connectDB(++attempts)
+      }
+
+      this.LOGS.error('Maximum attempts reached. Stopping service.')
+      process.exit(1)
     }
+  }
+
+  async onModuleInit () {
+    await this.connectDB()
   }
 
   public static getInstance () {
     if (!ConnectDB.instance) ConnectDB.instance = new ConnectDB()
-
     return ConnectDB.instance
   }
 
