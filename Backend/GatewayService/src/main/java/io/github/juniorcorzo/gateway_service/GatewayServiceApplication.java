3fe1af6dd937f6414518bf45039c223cb8ebc081
JuniorCorzo <josedanielmmf@gmail.com>
2024-11-30 22:27:20 -0500

    FEAT: Update application properties for Eureka, Gateway, Instruments, and TagsInstruments services; enhance service registration and routing configurations

--- Backend/GatewayService/src/main/java/io/github/juniorcorzo/gateway_service/GatewayServiceApplication.java
+++ Backend/GatewayService/src/main/java/io/github/juniorcorzo/gateway_service/GatewayServiceApplication.java
@@ -1,11 +1,11 @@
 package io.github.juniorcorzo.gateway_service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
