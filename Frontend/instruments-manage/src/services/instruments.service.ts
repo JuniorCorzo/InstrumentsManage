d327d9b5eb5d0ae3d6a4913730d4be6a109e2cc9
Junior Corzo <josedanielmmf@gmail.com>
2024-12-17 18:51:31 -0500

    FEAT: Implement getAllInstruments function to retrieve and adapt instrument data from the API

---
+++ Frontend/instruments-manage/src/services/instruments.service.ts
@@ -0,0 +1,15 @@
+import { instrumentAdapter } from "@/adapters/instruments.adapter"
+import { InstrumentDomain } from "@/interfaces/instrument-domain.interface"
+import { RetrieveDataDTO } from "@/interfaces/retrieve-data.interface"
+import { InstrumentDTO } from "@/models"
+import axios from "axios"
+
+export const getAllInstruments = async (): Promise<InstrumentDomain[]> => {
+    const response: RetrieveDataDTO = await axios.get("http://localhost:8080/instruments/all")
+     .then(response => {
+        if (response.status !== 200) throw Error(response.statusText)
+        return response.data
+     })
+
+    return response.data.map(instrument => instrumentAdapter(instrument as InstrumentDTO))
+}
