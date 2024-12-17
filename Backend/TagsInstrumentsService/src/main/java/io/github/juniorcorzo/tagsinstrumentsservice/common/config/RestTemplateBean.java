d8a09966708183361820308bef77b9c92126089d
Junior Corzo <josedanielmmf@gmail.com>
2024-12-16 20:09:14 -0500

    FIX: Add @Bean

--- Backend/TagsInstrumentsService/src/main/java/io/github/juniorcorzo/tagsinstrumentsservice/common/config/RestTemplateBean.java
+++ Backend/TagsInstrumentsService/src/main/java/io/github/juniorcorzo/tagsinstrumentsservice/common/config/RestTemplateBean.java
@@ -1,16 +1,14 @@
 package io.github.juniorcorzo.tagsinstrumentsservice.common.config;
 
-import org.apache.http.entity.ContentType;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestClientBean {
+public class RestTemplateBean {
 
-    @Bean("FOR_INSTRUMENTS_SERVICE")
+    @Bean
     @LoadBalanced
     public RestTemplate restClient() {
         return new RestTemplate();
