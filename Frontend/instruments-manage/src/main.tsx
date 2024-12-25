49ca28097e08ab938e38405a5ee04681acc25e47
Junior Corzo <josedanielmmf@gmail.com>
2024-12-25 18:29:38 -0500

    FEAT: Add axios for API calls and implement CRUD operations for instruments

--- Frontend/instruments-manage/src/main.tsx
+++ Frontend/instruments-manage/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/home/home.page'
-import Layout from './layouts/Layout'
 import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
--- Frontend/instruments-manage/src/services/instruments.service.ts
+++ Frontend/instruments-manage/src/services/instruments.service.ts
@@ -1,15 +1,47 @@
-import { instrumentAdapter } from "@/adapters/instruments.adapter"
-import { InstrumentDomain } from "@/interfaces/instrument-domain.interface"
-import { RetrieveDataDTO } from "@/interfaces/retrieve-data.interface"
-import { InstrumentDTO } from "@/models"
 import axios from "axios"
 
+import { instrumentAdapter } from "../adapters/instruments.adapter"
+import { InstrumentDomain } from "../interfaces/instrument-domain.interface"
+import { RetrieveDataDTO } from "../interfaces/retrieve-data.interface"
+import { InstrumentDTO } from "../models"
+import { GATEWAY_HOST } from "../config/env.config"
+
 export const getAllInstruments = async (): Promise<InstrumentDomain[]> => {
-    const response: RetrieveDataDTO = await axios.get("http://localhost:8080/instruments/all")
+    const response: RetrieveDataDTO = await axios.get(`${GATEWAY_HOST}/instruments/all`)
      .then(response => {
         if (response.status !== 200) throw Error(response.statusText)
         return response.data as RetrieveDataDTO
      })
 
     return response.data.map(instrument => instrumentAdapter(instrument as InstrumentDTO))
+}
+
+export const getInstrumentById = async (id: string): Promise<InstrumentDomain> => {
+    const response = await axios.get(`${GATEWAY_HOST}/instruments/${id}`).then(response => {
+        if (response.status !== 200) throw Error(response.statusText)
+        return response.data as RetrieveDataDTO
+    })
+
+    return instrumentAdapter(response.data[0] as InstrumentDTO)
+}
+
+export const createInstruments = async (instrument: InstrumentDomain) => {
+    axios.post(`${GATEWAY_HOST}/instruments/create`, instrument)
+    .then( response => {
+        if (response.status !== 201) throw Error(response.statusText)
+    })
+}
+
+export const updateInstruments = async (instrument: InstrumentDomain) => {
+    axios.put(`${GATEWAY_HOST}/instruments/update`, instrument)
+    .then( response => {
+        if (response.status !== 200) throw Error(response.statusText)
+    })
+}
+
+export const deleteInstruments = async (id: string) => {
+    axios.delete(`${GATEWAY_HOST}/instruments/delete/${id}`)
+    .then( response => {
+        if (response.status !== 200) throw Error(response.statusText)
+    })
 }
