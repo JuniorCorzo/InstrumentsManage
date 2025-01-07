9c60df37d5fab725e49dc682dccdeb1e704ec7aa
Junior Corzo <josedanielmmf@gmail.com>
2025-01-07 17:25:15 -0500

    REFAC: Simplify WriteBatchBrand service by removing unused repository dependencies and improving method readability; enhance updateBrands method for better clarity in bulk operations.

--- Backend/InstrumentsService/src/main/java/io/github/juniorcorzo/InstrumentsService/brands/services/WriteBatchBrand.java
+++ Backend/InstrumentsService/src/main/java/io/github/juniorcorzo/InstrumentsService/brands/services/WriteBatchBrand.java
@@ -1,20 +1,12 @@
 package io.github.juniorcorzo.InstrumentsService.brands.services;
 
-
 import io.github.juniorcorzo.InstrumentsService.brands.models.Brands;
-import io.github.juniorcorzo.InstrumentsService.brands.repositories.BrandsRepository;
 import io.github.juniorcorzo.InstrumentsService.brands.validations.BrandsValidations;
 import io.github.juniorcorzo.InstrumentsService.shared.dto.ResponseWithoutData;
 import io.github.juniorcorzo.InstrumentsService.instruments.models.Instruments;
-import io.github.juniorcorzo.InstrumentsService.instruments.repositories.InstrumentsRepository;
 import io.github.juniorcorzo.InstrumentsService.shared.utils.ResponseMessages;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.BulkOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -24,27 +16,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 public class WriteBatchBrand {
-    private final BrandsRepository brandsRepository;
-    private final InstrumentsRepository instrumentsRepository;
     private final BrandsValidations brandsValidations;
     private final MongoTemplate mongoTemplate;
 
     private final Logger LOGS = LoggerFactory.getLogger(WriteBatchBrand.class);
 
-
-    @Autowired
-    public WriteBatchBrand(BrandsRepository brandsRepository, InstrumentsRepository instrumentsRepository, BrandsValidations brandsValidations, MongoTemplate mongoTemplate) {
-        this.brandsRepository = brandsRepository;
-        this.instrumentsRepository = instrumentsRepository;
+    public WriteBatchBrand(BrandsValidations brandsValidations, MongoTemplate mongoTemplate) {
         this.brandsValidations = brandsValidations;
         this.mongoTemplate = mongoTemplate;
     }
 
     @Transactional
-    public ResponseWithoutData updateBrands(String idBrand, String newName){
+    public ResponseWithoutData updateBrands(String idBrand, String newName) {
         LOGS.info("Updating brand name in the both brands and instruments documents");
         this.brandsValidations.validIdWithBrandExists(idBrand);
 
@@ -54,16 +39,15 @@ public class WriteBatchBrand {
         bulkOperationsBrand.updateOne(queryBrand, updateBrand);
         bulkOperationsBrand.execute();
 
-        BulkOperations bulkOperationsInstruments = mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED, Instruments.class);
+        BulkOperations bulkOperationsInstruments = mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED,
+                Instruments.class);
         bulkOperationsInstruments.updateMulti(
-            new Query(Criteria.where("brand.id").is(idBrand)),
-            new Update().set("brand.name", newName)
-        );
+                new Query(Criteria.where("brand.id").is(idBrand)),
+                new Update().set("brand.name", newName));
         bulkOperationsInstruments.execute();
 
         return new ResponseWithoutData(
                 HttpStatus.OK,
-                ResponseMessages.OK.getMessage()
-        );
+                ResponseMessages.OK.getMessage());
     }
 }
