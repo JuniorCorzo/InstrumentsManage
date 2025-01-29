8f6f4cc9430360418d37705b7e5132d33012be99
juniorcorzo <josedanielmmf@gmail.com>
2025-01-29 18:18:57 -0500

    Refactor response DTO and unit process model for improved consistency and clarity

--- Backend/unit-process-service/src/common/repository/mongo-repository.repository.ts
+++ Backend/unit-process-service/src/common/repository/mongo-repository.repository.ts
@@ -1,5 +1,5 @@
 import { Inject } from '@nestjs/common'
-import { Collection, Filter, ObjectId, OptionalUnlessRequiredId, WithId } from 'mongodb'
+import { Collection, ObjectId, OptionalUnlessRequiredId, WithId } from 'mongodb'
 import { ConnectDB } from 'src/common/config/ConnectDB'
 
 // TODO:: Mejorar el manejo de excepciones
@@ -21,9 +21,9 @@ export class MongoRepository<T, ID> {
     }
   }
 
-  public async findById (id: ID): Promise<WithId<T>> {
+  public async findById (id: string): Promise<WithId<T>> {
     try {
-      return await this.collection.findOne({ _id: new ObjectId(id as string) as Filter<T> })
+      return await this.collection.findOne({ _id: new ObjectId(id) as ID })
     } catch (error) {
       console.error(error)
     }
@@ -31,10 +31,10 @@ export class MongoRepository<T, ID> {
 
   public async insert (document: OptionalUnlessRequiredId<T>): Promise<WithId<T>> {
     try {
-      const idInsert: ID = await this.collection.insertOne(document)
-        .then(({ insertedId }) => insertedId.toString() as ID)
+      const idInsert: string = await this.collection.insertOne(document)
+        .then(({ insertedId }) => insertedId.toString())
 
-      if (!ObjectId.isValid(idInsert as string)) throw new Error('Error al momento de insertar')
+      if (!ObjectId.isValid(idInsert)) throw new Error('Error al momento de insertar')
 
       return await this.findById(idInsert)
     } catch (error) {
@@ -44,26 +44,26 @@ export class MongoRepository<T, ID> {
 
   public async update (document: WithId<T>): Promise<WithId<T>> {
     try {
-      const { _id, ...documentWithoutId } = document
+      const { id, ...withoutIdDocument } = document
 
-      return await this.collection.updateOne(
-        { _id: new ObjectId(_id) as Filter<T> },
-        { $set: documentWithoutId as Partial<T> })
-        .then(({ matchedCount }) => {
-          if (matchedCount >= 1) return this.findById(_id as ID)
-          throw new Error('Error al intentar actualizar')
-        })
+      const responseUnitProcess = await this.collection.updateOne(
+        { _id: new ObjectId(id) as ID },
+        { $set: withoutIdDocument as Partial<T> }
+      )
+      if (responseUnitProcess.modifiedCount === 0) { throw new Error('Error in update') }
+
+      return await this.findById(id.toString())
     } catch (error) {
-      console.error(error)
+      console.error(error.message)
     }
   }
 
   public async delete (id: ID) {
     await this.collection
-      .deleteOne({ _id: new ObjectId(id as string) } as WithId<T>)
+      .deleteOne({ id: new ObjectId(id as string) } as WithId<T>)
   }
 
-  public async existById (id: ID): Promise<boolean> {
+  public async existById (id: string): Promise<boolean> {
     try {
       const data = await this.findById(id)
       return data === null
