87446d335814ab2823391697ef0418be81b769f6
juniorcorzo <josedanielmmf@gmail.com>
2025-01-30 17:50:50 -0500

    Refactor camp and unit process services to add logging for database operations; improve traceability

--- Backend/unit-process-service/src/camp/services/camp.service.ts
+++ Backend/unit-process-service/src/camp/services/camp.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable } from '@nestjs/common'
+import { HttpStatus, Injectable, Logger } from '@nestjs/common'
 import { CampRepository } from '../repositories/camp.repository'
 import { Camp } from '../model/camp.model'
 import { WithId } from 'mongodb'
@@ -7,12 +7,15 @@ import { ResponseMessages } from 'src/common/enums/response-messages'
 
 @Injectable()
 export class CampService {
+  private readonly logs = new Logger(CampService.name, { timestamp: true })
+
   constructor (
         private readonly campRepository: CampRepository
 
   ) {}
 
   public async getAllCamps (): Promise<ResponseDTO<Camp>> {
+    this.logs.log('[GET] Fetching all camps records from MongoDB')
     const campData = await this.campRepository.findAll()
     return new ResponseDTO(
       HttpStatus.OK,
@@ -22,6 +25,7 @@ export class CampService {
   }
 
   public async getCampById (id: string): Promise<ResponseDTO<Camp>> {
+    this.logs.log(`[GET] Fetch the camp with record ${id} of MongoDB`)
     const campData = await this.campRepository.findById(id)
 
     return new ResponseDTO<Camp>(
@@ -32,7 +36,9 @@ export class CampService {
   }
 
   public async insertCamp (camp: WithId<Camp>): Promise<ResponseDTO<Camp>> {
+    this.logs.log('[POST] Inserting new document of camp to MongoDB')
     const dataInsert = await this.campRepository.save(camp)
+    this.logs.log('Inserted new camp document is successfully')
 
     return new ResponseDTO(
       HttpStatus.CREATED,
@@ -42,17 +48,21 @@ export class CampService {
   }
 
   public async updateCamp (camp: WithId<Camp>): Promise<ResponseDTO<Camp>> {
-    const dataUpdated = await this.campRepository.save(camp)
+    this.logs.log(`[PUT] Updating camp document with the id ${camp.id} of MongoDB`)
+    const campUpdated = await this.campRepository.save(camp)
+    this.logs.log(`Updated camp document with id ${camp.id} is successfully`)
 
     return new ResponseDTO(
       HttpStatus.OK,
       ResponseMessages.OK,
-      [dataUpdated]
+      [campUpdated]
     )
   }
 
   public async deleteCamp (id: string): Promise<ResponseDTO<Camp>> {
+    this.logs.log(`[DELETE] Deleting camp document with the id ${id} of MongoDB`)
     await this.campRepository.delete(id)
+    this.logs.log(`Deleted camp document with the id ${id} is successfully`)
 
     return new ResponseDTO(
       HttpStatus.NO_CONTENT,
--- Backend/unit-process-service/src/unitProcess/service/unit-process.service.ts
+++ Backend/unit-process-service/src/unitProcess/service/unit-process.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable } from '@nestjs/common'
+import { HttpStatus, Injectable, Logger } from '@nestjs/common'
 import { UnitProcessRepository } from 'src/unitProcess/repositories/unit-process.repository'
 import { UnitProcess } from '../model/unit-process.model'
 import { WithId } from 'mongodb'
@@ -7,11 +7,14 @@ import { ResponseDTO } from 'src/common/dto/response.dto'
 
 @Injectable()
 export class UnitProcessService {
+  private readonly logs = new Logger(UnitProcess.name, { timestamp: true })
+
   constructor (
     private readonly unitProcessRepository: UnitProcessRepository
   ) {}
 
   public async getAllUnitProcess (): Promise<ResponseDTO<WithId<UnitProcess>>> {
+    this.logs.log('[GET] Fetching all process unit records from MongoDB')
     const data = await this.unitProcessRepository.findAll()
     return new ResponseDTO(
       'OK',
@@ -21,8 +24,8 @@ export class UnitProcessService {
   }
 
   public async getUnitProcessById (id: string): Promise<ResponseDTO<UnitProcess>> {
+    this.logs.log(`[GET] Fetch the process unit with record ${id} of MongoDB`)
     const data = await this.unitProcessRepository.findById(id)
-
     return new ResponseDTO(
       'OK',
       ResponseMessages.OK,
@@ -31,11 +34,14 @@ export class UnitProcessService {
   }
 
   public async existById (id: string): Promise<boolean> {
+    this.logs.log(`Verify if exist with record ${id} of MongoDB`)
     return await this.unitProcessRepository.existById(id)
   }
 
   public async insertUnitProcess (unitProcess: UnitProcess): Promise<ResponseDTO<UnitProcess>> {
+    this.logs.log('[POST] Inserting new document of process unit of MongoDB')
     const saveInsert = await this.unitProcessRepository.save(unitProcess)
+    this.logs.log('Inserted new process unit document is successfully')
 
     return new ResponseDTO<UnitProcess>(
       HttpStatus.CREATED,
@@ -45,18 +51,20 @@ export class UnitProcessService {
   }
 
   public async updateUnitProcess (unitProcess: WithId<UnitProcess>): Promise<ResponseDTO<UnitProcess>> {
-    const saveInsert = await this.unitProcessRepository.save(unitProcess)
-
+    this.logs.log(`[PUT] Updating process unit document with the id ${unitProcess.id} of MongoDB`)
+    const unitProcessUpdated = await this.unitProcessRepository.save(unitProcess)
+    this.logs.log(`Updated process unit document with id ${unitProcess.id} is successfully`)
     return new ResponseDTO<UnitProcess>(
       HttpStatus.OK,
       ResponseMessages.OK,
-      [saveInsert]
+      [unitProcessUpdated]
     )
   }
 
   public async deleteUnitProcess (id: string): Promise<ResponseDTO<UnitProcess>> {
+    this.logs.log(`[DELETE] Deleting process unit document with the id ${id} of MongoDB`)
     await this.unitProcessRepository.delete(id)
-
+    this.logs.log(`Deleted process unit document with the id ${id} is successfully`)
     return new ResponseDTO<UnitProcess>(
       HttpStatus.NO_CONTENT,
       ResponseMessages.DELETE
