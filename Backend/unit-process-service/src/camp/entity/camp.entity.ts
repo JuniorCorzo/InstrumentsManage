00ae784dde88746e090864908dc2855c698423cf
JuniorCorzo <josedanielmmf@gmail.com>
2025-02-05 19:07:28 -0500

    FEAT: Introduce DTOs and adapters for Camp and UnitProcess; enhance data handling and validation logic

--- Backend/unit-process-service/src/camp/entity/camp.entity.ts
+++ Backend/unit-process-service/src/camp/entity/camp.entity.ts
@@ -1,16 +1,16 @@
-import { ObjectId } from 'mongodb'
-import { Collection } from 'src/common/decorator/collection.decorator'
-import { Location } from '../interfaces/location.interface'
+import { ObjectId } from "mongodb";
+import { Collection } from "src/common/decorator/collection.decorator";
+import { Location } from "../interfaces/location.interface";
 
 type ICampEntity = {
-    _id?: ObjectId
-    name: string
-    location: Location
-}
+  _id?: ObjectId;
+  name: string;
+  location: Location;
+};
 
-@Collection('Camps')
+@Collection("Camps")
 export class CampEntity implements ICampEntity {
-  _id: ObjectId
-  name: string
-  location: Location
+  _id: ObjectId;
+  name: string;
+  location: Location;
 }
