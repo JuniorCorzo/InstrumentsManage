3dfdcc7fcdff2d069fd21c1ae84db579d55ad6f2
JuniorCorzo <josedanielmmf@gmail.com>
2025-02-12 18:51:29 -0500

    FEAT: Refactor transformToString utility for improved string formatting

--- Frontend/instruments-manage/src/utils/transform-string.utils.ts
+++ Frontend/instruments-manage/src/utils/transform-string.utils.ts
@@ -1,6 +1,9 @@
-export const transformToString = (arrayConvert: string[] | number[]): string => {
-    return arrayConvert.map((str, index) => {
-        if (arrayConvert.length -1 !== index) { return `${str}, ` }
-        return str
-    }).join()
-}
+export const transformToString = (
+  arrayConvert: string[] | number[]
+): string => {
+  return arrayConvert
+    .map((str) => {
+      return str;
+    })
+    .join(", ");
+};
