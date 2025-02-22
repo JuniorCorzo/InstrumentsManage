557b0afcc0bfeaf52fc3f951f864a3f6b87674b3
JuniorCorzo <josedanielmmf@gmail.com>
2025-02-21 19:53:23 -0500

    REFAC: Change CacheNode timestamp type from Timestamp to Long and update timestamp initialization

--- Backend/TagsInstrumentsService/src/main/java/io/github/juniorcorzo/tagsinstrumentsservice/orchestrator/cache/CacheNode.java
+++ Backend/TagsInstrumentsService/src/main/java/io/github/juniorcorzo/tagsinstrumentsservice/orchestrator/cache/CacheNode.java
@@ -2,25 +2,22 @@ package io.github.juniorcorzo.tagsinstrumentsservice.orchestrator.cache;
 
 import lombok.Data;
 
-import java.sql.Timestamp;
 import java.time.Instant;
 
-
 @Data
-class CacheNode<T> {
+public class CacheNode<T> {
     private T item;
-    private Timestamp timestamp;
+    private Long timestamp;
 
-    public CacheNode (T item){
+    public CacheNode(T item) {
         this.item = item;
-        this.timestamp = Timestamp.from(Instant.now());
+        this.timestamp = Instant.now().getEpochSecond();
     }
 
-    public T getItem () {
-        this.timestamp = Timestamp.from(Instant.now());
+    public T getItem() {
+        this.timestamp = Instant.now().getEpochSecond();
         return item;
     }
-
 
     public CacheNode<T> setItem(T item) {
         this.item = item;
