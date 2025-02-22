c2bc4bc43b494a223c4d7e22381f5545d6e1b48a
JuniorCorzo <josedanielmmf@gmail.com>
2025-02-22 17:00:25 -0500

    Refactor: Enhance InstrumentService and UnitProcessService with improved error handling and logging for API requests

--- Backend/TagsInstrumentsService/src/main/java/io/github/juniorcorzo/tagsinstrumentsservice/orchestrator/services/InstrumentService.java
+++ Backend/TagsInstrumentsService/src/main/java/io/github/juniorcorzo/tagsinstrumentsservice/orchestrator/services/InstrumentService.java
@@ -6,12 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class InstrumentService {
@@ -28,24 +31,41 @@ public class InstrumentService {
 
     public List<InstrumentsDTO> getAllInstruments() {
         this.LOGS.info("Fetching all instruments");
-        return Optional.ofNullable(
-                        this.restTemplate.exchange("lb://INSTRUMENTS-SERVICE/instruments/all", HttpMethod.GET,
-                                null,
-                                this.responseType).getBody())
-                .map(RetrieveDTO::data)
-                .orElse(List.of());
+        try {
+            ResponseEntity<RetrieveDTO<InstrumentsDTO>> response = this.restTemplate.exchange(
+                    "lb://INSTRUMENTS-SERVICE/instruments/all",
+                    HttpMethod.GET,
+                    null,
+                    this.responseType);
+            if (response.getStatusCode() != HttpStatus.OK) {
+                throw new IOException("Unexpected Code: " + response.getStatusCode());
+            }
+            List<InstrumentsDTO> instrumentsResponse = Objects.requireNonNull(response.getBody().data());
+            return instrumentsResponse;
+        } catch (Exception e) {
+            this.LOGS.info("Error to send request at instruments service: {}", e.getMessage());
+        }
+
+        return Collections.emptyList();
     }
 
     public InstrumentsDTO getInstrumentById(String id) {
         this.LOGS.info("Fetching instrument with id {}", id);
-        return Objects.requireNonNull(
-                        this.restTemplate
-                                .exchange("lb://INSTRUMENTS-SERVICE/instruments?id={id}",
-                                        HttpMethod.GET, null,
-                                        this.responseType,
-                                        id
-                                ).getBody())
-                .data()
-                .getFirst();
+        try {
+            ResponseEntity<RetrieveDTO<InstrumentsDTO>> response = this.restTemplate
+                    .exchange("lb://INSTRUMENTS-SERVICE/instruments?id={id}",
+                            HttpMethod.GET, null,
+                            this.responseType,
+                            id);
+            if (response.getStatusCode() != HttpStatus.OK) {
+                throw new IOException("Unexpected Code: " + response.getStatusCode());
+            }
+            InstrumentsDTO instrumentResponse = Objects.requireNonNull(response.getBody().data()).getFirst();
+            return instrumentResponse;
+        } catch (IOException e) {
+            this.LOGS.error("Error to send request at instruments service: {}", e.getMessage());
+        }
+
+        return null;
     }
 }
